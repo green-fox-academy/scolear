9c5b9cd7a31fcc83e8ef05ba59a4b903bbef89e5
scolear <bobtera@gmail.com>
2019-09-11 11:59:54 +0200

    organize project folders

---
+++ week-01/day-03/IntroduceY-TwoNums-CodingH/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include <iostream>
+
+using std::cout;
+
+int main(int argc, char const *argv[])
+{
+    // Write a program that prints a few details to the terminal window about you
+    // It should print each detail to a new line.
+    //  - Your name
+    //  - Your age
+    //  - Your height in meters (as a decimal fraction)
+    //
+    //  Example output:
+    //  John Doe
+    //  31
+    //  1.87
+
+    int age = 28;
+    float height = 1.82;
+
+    cout << "Robert Tera \n";
+    cout << age << std::endl;
+    cout << height;
+
+
+
+    return 0;
+}
--- week-01/day-03/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(int argc, char const *argv[]) {
-
-    // Modify this program to greet you instead of the World!
-    std::cout << "Hello scolear!" << std::endl;
-
-    return 0;
-}
