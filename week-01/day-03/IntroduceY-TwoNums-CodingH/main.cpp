fcb344df255825ff138e28cba264c0ab6b921ff1
scolear <bobtera@gmail.com>
2019-09-11 13:02:53 +0200

    introduce-twoN-codingH

--- week-01/day-03/IntroduceY-TwoNums-CodingH/main.cpp
+++ week-01/day-03/IntroduceY-TwoNums-CodingH/main.cpp
@@ -7,7 +7,7 @@ using std::cout;
 int main(int argc, char const *argv[])
 {
     /*
-     
+
     // Write a program that prints a few details to the terminal window about you
     // It should print each detail to a new line.
     //  - Your name
