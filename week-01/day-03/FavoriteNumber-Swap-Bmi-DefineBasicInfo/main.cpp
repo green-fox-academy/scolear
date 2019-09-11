c9b999ab2a07217049bc1d06a556c4f9145b7cef
scolear <bobtera@gmail.com>
2019-09-11 13:21:27 +0200

    complete fav-swap-bmi-defbas

---
+++ week-01/day-03/FavoriteNumber-Swap-Bmi-DefineBasicInfo/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    /*// Store your favorite number in a variable (as an integer)
+    // And print it like this: "My favorite number is: 8"
+
+    int favorite = 4;
+
+    std::cout << "My favorite number is :" << favorite;
+
+    return 0;*/
+
+
+    /*// Swap the values of the variables
+    int a = 123;
+    int b = 526;
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+
+    return 0;*/
+
+
+    /*double massInKg = 81.2;
+    double heightInM = 1.78;
+
+    // Print the Body mass index (BMI) based on these values
+
+    double BMI = 81.2 / (1.78 * 1.78);
+
+    std::cout << "BMI is: " << BMI;
+
+    return 0;*/
+
+
+    // Define several things as a variable then print their values
+    // Your name as a string
+    // Your age as an integer
+    // Your height in meters as a double
+    // Whether you are married or not as a boolean
+
+    std::string name = "Robert";
+    int age = 28;
+    double height = 1.82;
+    bool married = false;
+
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "Height: " << height << std::endl;
+    std::cout << "Married: " << married << std::endl;
+
+    return 0;
+
+}
