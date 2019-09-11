a37fe66807db1c0bad7a8f1dd857af42e8bbbba0
scolear <bobtera@gmail.com>
2019-09-11 21:43:48 +0200

    finally solve DrawPyramid

--- week-01/day-03/DrawPyramid/main.cpp
+++ week-01/day-03/DrawPyramid/main.cpp
@@ -18,12 +18,13 @@ int main(int argc, char* args[]) {
     std::cout << "Enter a number: ";
     std::cin >> num;
 
-    for (int i = 0; i < num; i++) {
-        for (int j = 0; j < num - 1; j++) {
+    for (int i = 1; i <= num; i++) {
+        for (int j = i; j < num; j++) {
             std::cout << " ";
         }
-
-
+        for (int j = 1; j <= (2 * i - 1); j++){
+            std::cout << "*";
+        }
         std::cout << std::endl;
     }
 
