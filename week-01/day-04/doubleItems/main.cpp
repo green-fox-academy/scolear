3b2de010d584b2abd6db1918850deeed234264d4
scolear <bobtera@gmail.com>
2019-09-12 16:47:13 +0200

    finish doubleItems

---
+++ week-01/day-04/doubleItems/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `numList`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Double all the values in the array
+
+    int numList[] = {3, 4, 5, 6, 7};
+
+    for (int i = 0; i < sizeof(numList) / sizeof(numList[0]); ++i) {
+        numList[i] *= 2;
+        std::cout << numList[i] << std::endl;
+    }
+
+    return 0;
+}
