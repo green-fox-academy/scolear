a0d8a97c4bfa1f1390f7314f553eb73841a966ef
scolear <bobtera@gmail.com>
2019-09-17 16:06:43 +0200

    finish middlelines

--- week-02/day-03/drawing-setup/draw.cpp
+++ week-02/day-03/drawing-setup/draw.cpp
@@ -1,4 +1,26 @@
  #include "draw.h"
 
  void draw(SDL_Renderer* gRenderer) {
+
+     // draw a red horizontal line to the canvas' middle.
+     // draw a green vertical line to the canvas' middle.
+
+     SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 255);
+     SDL_RenderDrawLine(gRenderer,0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+
+     SDL_SetRenderDrawColor(gRenderer, 0, 255, 0, 255);
+     SDL_RenderDrawLine(gRenderer,SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT);
+
+ }
+
+ int randomNumber(int nr_min, int nr_max)
+ {
+     static bool initialized = false;
+
+     if (!initialized) {
+         initialized = true;
+         srand(time(NULL));
+     }
+
+     return rand() % nr_max + nr_min;
  }
--- week-02/day-03/drawing-setup/draw.h
+++ week-02/day-03/drawing-setup/draw.h
@@ -2,10 +2,13 @@
 #define GFA_SDL2_DRAW_H
 
 #include <SDL.h>
+#include <time.h>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+
+int randomNumber(int nr_min, int nr_max);
 
 void draw(SDL_Renderer* gRenderer);
 
