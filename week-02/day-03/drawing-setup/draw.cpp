1862bcb6d32526aea28a4afd5024dfb9487e8d8e
scolear <bobtera@gmail.com>
2019-09-17 18:18:35 +0200

    ready todraw

--- week-02/day-03/drawing-setup/draw.cpp
+++ week-02/day-03/drawing-setup/draw.cpp
@@ -1,15 +1,21 @@
  #include "draw.h"
 
  void draw(SDL_Renderer* gRenderer) {
-
+/*
      // draw a red horizontal line to the canvas' middle.
      // draw a green vertical line to the canvas' middle.
 
      SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 255);
-     SDL_RenderDrawLine(gRenderer,0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+     SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
 
      SDL_SetRenderDrawColor(gRenderer, 0, 255, 0, 255);
-     SDL_RenderDrawLine(gRenderer,SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT);
+     SDL_RenderDrawLine(gRenderer, SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT);
+*/
+
+     // Draw a box that has different colored lines on each edge.
+     // The center of the box should align with the center of the screen.
+
+
 
  }
 
