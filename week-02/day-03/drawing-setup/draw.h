02087f42c5d818e231abd85a72ae99547921434a
scolear <bobtera@gmail.com>
2019-09-17 14:59:23 +0200

    setup of drawing environment completion

---
+++ week-02/day-03/drawing-setup/draw.h
@@ -0,0 +1,12 @@
+#ifndef GFA_SDL2_DRAW_H
+#define GFA_SDL2_DRAW_H
+
+#include <SDL.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+void draw(SDL_Renderer* gRenderer);
+
+#endif //GFA_SDL2_DRAW_H
