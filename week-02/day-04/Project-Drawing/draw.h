ea9844e7676c893d7343201086c46674b4c191c4
scolear <bobtera@gmail.com>
2019-09-18 17:13:31 +0200

    complete setup and finish lineplay

---
+++ week-02/day-04/Project-Drawing/draw.h
@@ -0,0 +1,14 @@
+#ifndef GFA_SDL2_DRAW_H
+#define GFA_SDL2_DRAW_H
+
+#include <SDL.h>
+
+//Screen dimension constants
+const int SCREEN_SIZE = 640;
+const int SCREEN_WIDTH = SCREEN_SIZE;
+const int SCREEN_HEIGHT = SCREEN_SIZE;
+
+
+void draw(SDL_Renderer* gRenderer);
+
+#endif //GFA_SDL2_DRAW_H
---
+++ week-02/day-04/Project-Drawing/linePlay.cpp
@@ -0,0 +1,20 @@
+//
+// Created by scolear on 2019. 09. 18..
+//
+
+#include "draw.h"
+
+void draw(SDL_Renderer* gRenderer) {
+
+    SDL_SetRenderDrawColor(gRenderer, 0, 255, 0, 255);
+
+    for (int i = 0; i < SCREEN_SIZE; i += 20) {
+        SDL_RenderDrawLine(gRenderer, i, 0, SCREEN_SIZE, i);
+    }
+
+    SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 255);
+    for (int i = 0; i < SCREEN_SIZE; i += 20) {
+        SDL_RenderDrawLine(gRenderer, 0, i, i, SCREEN_SIZE);
+    }
+    
+}
