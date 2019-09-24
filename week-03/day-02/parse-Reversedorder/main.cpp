a6a4c0287eaa4010142294e4b5c734795764e597
scolear <bobtera@gmail.com>
2019-09-24 16:50:23 +0200

    finish reversed-order

--- week-03/day-02/parse-Reversedorder/main.cpp
+++ week-03/day-02/parse-Reversedorder/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 int main() {
     // Create a program that decrypts the file called "reversed-order.txt",
@@ -8,14 +9,23 @@ int main() {
 
     std::ifstream inFile("../reversed-order.txt");
     std::string line;
+    std::vector<std::string> stringVector;
 
     if (inFile.is_open()) {
 
-
-
+        while (getline(inFile, line)) {
+            stringVector.push_back(line);
+        }
 
     } else {
         std::cout << "Unable to open file.";
     }
+
+    inFile.close();
+
+    for (int i = stringVector.size() - 1; i >= 0; --i) {
+        std::cout << stringVector[i] << std::endl;
+    }
+
     return 0;
 }
