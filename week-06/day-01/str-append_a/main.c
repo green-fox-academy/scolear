df9df132523ab8f416bc13534f4433fc989eceba
scolear <bobtera@gmail.com>
2019-10-21 16:28:52 +0200

    finish appenda with strcat

---
+++ week-06/day-01/str-append_a/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *append_a(char *string) {
+    return strcat(string, "a");
+}
+
+int main()
+{
+    // Create a program which asks for a string and stores it
+    // Create a function which takes a string as a parameter and
+    // appends a character 'a' to the end of it and returns the new string
+
+    char str1[30];
+
+    printf("Please enter a string:\n");
+    scanf("%s", str1);
+
+    printf("%s", append_a(str1));
+
+    // this does the same... :
+    // printf("%s\n", strcat(str1, "a"));
+
+    return 0;
+}
