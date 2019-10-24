8e8fa322b893e85ce0aeff610546a734ed8ef44e
scolear <bobtera@gmail.com>
2019-10-24 14:24:06 +0200

    finish houses

--- week-06/day-04/House/main.c
+++ week-06/day-04/House/main.c
@@ -23,12 +23,29 @@ typedef struct house {
     float area;
 } house_t, *house_p;
 
-bool worth_to_buy (house_p hpp) {
-    float price_per_sqm = 
+bool worth_to_buy (house_p hpp, int mp) {
+    float price_per_sqm = hpp->price / hpp->area;
+    return price_per_sqm < mp;
 }
 
 int main()
 {
+    house_t house1 = {"Ujbuda", 34000, 5, 80};
+    house_t house2 = {"Ujpest", 5000, 2, 30};
+    house_t house3 = {"Obuda", 12000, 5, 39};
+
     int market_price = 400;
+    house_t houses[3] = {house1, house2, house3};
+    int num_of_houses = sizeof(houses) / sizeof(houses[0]);
+    int count_of_worthit = 0;
+
+    for (int i = 0; i < num_of_houses; ++i) {
+        if (worth_to_buy(&houses[i], market_price)) {
+            printf("House in %s is worth to buy with a price/sqm of %.2f\n", houses[i].address, houses[i].price / houses[i].area);
+            count_of_worthit++;
+        }
+    }
+    printf("Houses worth to buy: %d", count_of_worthit);
+
     return 0;
 }
