1889ff3e411ad1a12053e60a3cdd0e8ed62dfde5
scolear <bobtera@gmail.com>
2019-10-28 10:56:03 +0100

    add calloc

--- week-07/day-01/Malloc/main.c
+++ week-07/day-01/Malloc/main.c
@@ -6,22 +6,38 @@
 // please allocate a 10 long array and fill it with 5!! numbers from 0 to 4, then print the whole array
 // please delete the array before the program exits
 
+// with calloc
+// please allocate a 10 long array and fill it with 5!! numbers from 0 to 4, then print the whole array
+// please delete the array before the program exits
+// what's the difference between this and the previous exercise?
 
 int main()
 {
     int* pointer = NULL;
 
+    // MALLOC:
     pointer = (int *) malloc(10 * sizeof(int));
 
     for (int i = 0; i < 5; ++i) {
         *(pointer + i) = i;
     }
-
+    printf("Malloc:\n");
 
     for (int i = 0; i < 10; ++i) {
-        printf("%d, ", *(pointer + i));
+        printf("%d\n", *(pointer + i));
     }
+    free(pointer);
 
+    // CALLOC:
+    pointer = (int *) calloc(10, sizeof(int));
+
+    for (int i = 0; i < 5; ++i) {
+        *(pointer + i) = i;
+    }
+    printf("Calloc:\n");
+    for (int i = 0; i < 10; ++i) {
+        printf("%d\n", *(pointer + i));
+    }
     free(pointer);
 
     return 0;
