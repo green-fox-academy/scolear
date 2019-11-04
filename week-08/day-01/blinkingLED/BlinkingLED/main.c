9c4d9a642dd8b92bc95ae06704bf0b90eff260c8
Robert Tera <bobtera@gmail.com>
2019-11-04 11:18:05 +0100

    initial setup commit

---
+++ week-08/day-01/blinkingLED/BlinkingLED/main.c
@@ -0,0 +1,18 @@
+/*
+ * BlinkingLED.c
+ *
+ * Created: 2019. 11. 04. 11:14:53
+ * Author : bobte
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
