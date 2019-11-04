3e391b54bc8a294845bdb0be36718b77e11493af
Robert Tera <bobtera@gmail.com>
2019-11-04 16:52:42 +0100

    finish knightrider effect

--- week-08/day-01/MultipleLEDs/MultipleLEDs/main.c
+++ week-08/day-01/MultipleLEDs/MultipleLEDs/main.c
@@ -2,6 +2,27 @@
 #define F_CPU 16000000
 #include <util/delay.h>
 
+void blink_at_1_Hz()
+{
+	PORTB = 0b00011110;
+	_delay_ms(500);
+	PORTB = 0b00000000;
+	_delay_ms(500);
+}
+
+void knight_rider()
+{
+	for (int i = 1; i < 5; i++)
+	{
+		PORTB = 1 << i;
+		_delay_ms(150);
+	}
+	for (int j = 3; j > 1; j--)
+	{
+		PORTB = 1 << j;
+		_delay_ms(150);
+	}
+}
 
 int main(void)
 {
@@ -9,10 +30,8 @@ int main(void)
 	
     while (1) 
     {
-		PORTB = 0b00011110;
-		_delay_ms(500);
-		PORTB = 0b00000000;
-		_delay_ms(500);
+		//blink_at_1_Hz();
+		knight_rider();
     }
 }
 
