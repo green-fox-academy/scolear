b1f05ddf508e5d10343aa95aee60627c95ecd231
Robert Tera <bobtera@gmail.com>
2019-11-05 16:09:36 +0100

    finish timer interrupt LED switcher

--- week-08/day-02/Timers/Timers/main.c
+++ week-08/day-02/Timers/Timers/main.c
@@ -1,4 +1,42 @@
 #include <avr/io.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
+
+uint8_t counter = 0;
+const uint8_t cntr_max = 30;
+
+// Write here the interrupt handler function. The vector name starts with TIMER ;). Use Ctrl+Space to find the proper vector name!
+// Do the same thing in the interrupt handler as before.
+// This time you don't have to do anything with the flags, the MCU does it automatically.
+
+ISR(TIMER0_OVF_vect)
+{
+    counter++;
+
+    if (counter >= cntr_max)
+    {
+        PINB = 1 << PINB5;
+        counter = 0;
+    }
+} 
+
+void manual_overflow_counter(uint8_t* counter, uint8_t counter_max)
+{
+    // If TC0 counter overflow occurred (TOV0 flag is set) toggle the LED and write "1" to that flag
+    // (this will clear it, it's confusing, but this is how it works)
+
+    if (TIFR0 & 0x01)
+    {
+        (*counter)++;
+        TIFR0 |= 1 << 0;
+    }
+
+    if (*counter >= counter_max)
+    {
+        PINB = 1 << PINB5;
+        *counter = 0;
+    }
+}
 
 void init()
 {
@@ -7,34 +45,17 @@ void init()
     TCCR0B |= 0b00000101;
     // Set the PB5 LED pin to output
     DDRB |= 1 << 5;
+    // Enable the TC0 overflow interrupt
+    TIMSK0 |= 1 << 0; 
+    // Enable the interrupts globally
+    sei();
 }
 
 int main(void)
 {
-    // Create a variable which will be a counter variable, initialize it with 0.
-    uint8_t counter = 0;
-    // This will be the maximal value of our counter
-    const uint8_t cntr_max = 30;
-
-    // Your task is to create about 1Hz flashing LED with the TC0 timer.
-    // Use the counter variable, cntr_max and the overflow flag as a hint.
-
     init();
 
     while (1)
     {
-        // If TC0 counter overflow occurred (TOV0 flag is set) toggle the LED and write "1" to that flag
-        // (this will clear it, it's confusing, but this is how it works)
-        if (TIFR0 & 0x01)
-        {
-            counter++;
-            TIFR0 |= 1 << 0;
-        }
-
-        if (counter >= cntr_max)
-        {
-            PINB = 1 << PINB5;
-            counter = 0;
-        }
     }
 }
