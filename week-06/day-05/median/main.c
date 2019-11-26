eeca3c8d5992426039afd02461846508c3a75e92
scolear <bobtera@gmail.com>
2019-11-26 15:58:48 +0100

    finish median exercise

--- week-06/day-05/median/main.c
+++ week-06/day-05/median/main.c
@@ -6,12 +6,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define HEIGHTS_BUFFER 20
+
 typedef struct group {
     char name[10];
-    int heights[50];
+    int heights[HEIGHTS_BUFFER];
+    int number_of_people;
 } group_t;
 
-int count_lines (char* filename) {
+int count_lines(char* filename)
+{
     FILE* fptr;
     if (!(fptr = fopen(filename, "r"))) return -1;
     int i = 0;
@@ -23,10 +27,42 @@ int count_lines (char* filename) {
     return i;
 }
 
-void init_heights_array (group_t *group, int length) {
+void init_heights_array(group_t *group, int length)
+{
     for (int i = 0; i < length; ++i) {
         group->heights[i] = 0;
     }
+}
+
+void swap(int *a, int *b)
+{
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
+
+void bubble_sort(group_t *group)
+{
+    int swap_flag = 1;
+    int i = 0;
+    while (swap_flag) {
+        swap_flag = 0;
+        i = 0;
+
+        while(i < group->number_of_people - 1) {
+            if(group->heights[i] > group->heights[i + 1]) {
+                swap(&group->heights[i], &group->heights[i + 1]);
+                swap_flag = 1;
+            }
+            i++;
+        }
+    }
+}
+
+int find_median(group_t *group)
+{
+    // in case of even number of people it returns higher median:
+    return group->heights[group->number_of_people / 2];
 }
 
 int main()
@@ -39,7 +75,8 @@ int main()
     group_t groups[number_of_groups];
 
     for (int i = 0; i < number_of_groups; ++i) {
-        init_heights_array(&groups[i], 50);
+        init_heights_array(&groups[i], HEIGHTS_BUFFER);
+        groups[i].number_of_people = 0;
     }
 
     FILE* fptr;
@@ -49,6 +86,7 @@ int main()
     int row_index = 0;
     char buffer[100];
     char *word;
+    int temp_data;
 
     // reading lines:
     while (fgets(buffer, 100, fptr)){
@@ -63,8 +101,12 @@ int main()
         word = strtok(NULL, ",");
 
         while (word != NULL) {
+            temp_data = atoi(word);
+            groups[line_index].heights[row_index] = temp_data;
 
-            groups[line_index].heights[row_index] = atoi(word);
+            if (temp_data != 0) {
+                groups[line_index].number_of_people++;
+            }
 
             word = strtok(NULL, ",");
             row_index++;
@@ -73,10 +115,17 @@ int main()
     }
 
     for (int j = 0; j < number_of_groups; ++j) {
-        printf("%s\n", groups[j].name);
+        printf("%s : ", groups[j].name);
+        bubble_sort(&groups[j]);
+        for (int k = 0; k < groups[j].number_of_people; ++k) {
+            printf("%d, ", groups[j].heights[k]);
+        }
+        printf("\n");
     }
 
-    // todo: find medians of heights arrays. Consider data ends at zeroes. We have to sort them first.
+    for (int j = 0; j < number_of_groups; ++j) {
+        printf("%s median: %d\n", groups[j].name, find_median(&groups[j]));
+    }
 
     return 0;
 }
