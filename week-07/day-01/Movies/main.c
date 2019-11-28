105bd62e40f3ed6490794edf2ac8ed880ef1a9af
scolear <bobtera@gmail.com>
2019-11-28 15:12:17 +0100

    finish movies. messy, copied, but done

--- week-07/day-01/Movies/main.c
+++ week-07/day-01/Movies/main.c
@@ -18,12 +18,14 @@ typedef struct Movie {
 
 typedef struct Year {
     int             year;
-    unsigned long   profit;
+    unsigned long   profit_sum;
 } year_t;
 
 int           count_lines(char* filename);
 unsigned long parse_profit(const char* profit_string);
-int           find_best_year(movie_t* movies, int number_of_movies);
+int           count_unique_years(movie_t* movies, int number_of_movies);
+year_t*       year_sums(movie_t* movies, int number_of_movies);
+int           find_best_year(year_t* year_stats, int years);
 
 int main()
 {
@@ -44,7 +46,6 @@ int main()
 
         word = strtok(NULL, " ");
         movies[row_index].profit = parse_profit(word);
-        printf("%lu\n", movies[row_index].profit);
 
         word = strtok(NULL, "\n");
         movies[row_index].year_released = atoi(word);
@@ -52,10 +53,11 @@ int main()
         row_index++;
     }
 
-    printf("%d", find_best_year(movies,number_of_movies));
+    year_t* year_stats = year_sums(movies, number_of_movies);
+
+    printf("Best year for movies: %d\n", find_best_year(year_stats, count_unique_years(movies, number_of_movies)));
     return 0;
 }
-
 
 int count_lines(char* filename)
 {
@@ -84,19 +86,57 @@ unsigned long parse_profit(const char* profit_string)
     return result;
 }
 
-int find_best_year(movie_t* movies, int number_of_movies)
+int count_unique_years(movie_t* movies, int number_of_movies)
 {
-    year_t* years = (year_t*) malloc(sizeof(year_t));
-    years[0].year = movies[0].year_released;
-    years[0].profit = movies[0].profit;
-
-    // todo: count unique years
-    for (int i = 1; i < number_of_movies; ++i) {
-
-
-
-
+    int diff_years = 0;
+    for (int i = 0; i < number_of_movies; i++) {
+        int match = 0;
+        for (int j = 0; j < i && ! match; j++) {
+            if (movies[i].year_released == movies[j].year_released) {
+                match++;
+            }
+        }
+        if (match == 0) {
+            diff_years++;
+        }
     }
+    return diff_years;
+}
 
+year_t* year_sums(movie_t* movies, int number_of_movies)
+{
+    int     number_of_years = count_unique_years(movies, number_of_movies);
+    year_t* yearly_stat = calloc(number_of_years, sizeof(year_t));
+    int     years = 0;
 
+    for (int i = 0; i < number_of_movies; ++i) {
+        int found = 0;
+        for (int j = 0; j < years && !found; ++j) {
+            if (movies[i].year_released == yearly_stat[j].year) {
+                yearly_stat[j].profit_sum += movies[i].profit;
+                found++;
+            }
+        }
+
+        if (!found) {
+            yearly_stat[years].year = movies[i].year_released;
+            yearly_stat[years].profit_sum = movies[i].profit;
+            years++;
+        }
+    }
+    return yearly_stat;
+}
+
+int find_best_year(year_t* year_stats, int years)
+{
+    unsigned long   max_income = 0;
+    int             best_year = 0;
+
+    for (int i = 0; i < years; ++i) {
+        if (max_income < year_stats[i].profit_sum) {
+            max_income = year_stats[i].profit_sum;
+            best_year = year_stats[i].year;
+        }
+    }
+    return best_year;
 }
