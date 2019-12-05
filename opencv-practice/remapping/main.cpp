92ca2f723a4f608cb9401c6fe30dc3ec504d595b
scolear <bobtera@gmail.com>
2019-12-05 10:52:56 +0100

    add histogram equalization to remapping project

--- opencv-practice/remapping/main.cpp
+++ opencv-practice/remapping/main.cpp
@@ -11,9 +11,6 @@ int main()
     cv::Mat map_x(src.size(), CV_32FC1);
     cv::Mat map_y(src.size(), CV_32FC1);
 
-    cv::namedWindow("Original", cv::WINDOW_AUTOSIZE);
-    cv::namedWindow("Remap test", cv::WINDOW_AUTOSIZE);
-
     /// Turning it upside down:
     for (int i = 0; i < map_x.rows; ++i) {
         for (int j = 0; j < map_x.cols; ++j) {
@@ -24,8 +21,23 @@ int main()
 
     cv::remap(src, dst, map_x, map_y, cv::INTER_LINEAR, cv::BORDER_CONSTANT, cv::Scalar(0, 0, 0));
 
+    cv::namedWindow("Original", cv::WINDOW_AUTOSIZE);
     cv::imshow("Original", src);
+
+    cv::namedWindow("Remap test", cv::WINDOW_AUTOSIZE);
     cv::imshow("Remap test", dst);
+
+    /// Histogram equalization:
+    // Grayscaling:
+    cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+
+    cv::namedWindow("Original Grayscale", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Original Grayscale", src);
+
+    cv::equalizeHist(src, dst);
+
+    cv::namedWindow("Equalized Histogram", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Equalized Histogram", dst);
 
     cv::waitKey();
 
