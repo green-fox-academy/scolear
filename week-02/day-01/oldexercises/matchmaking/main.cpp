f42c88ee7983992a29ce286894a1d1282cd66775
scolear <bobtera@gmail.com>
2020-01-23 14:31:29 +0100

    valami

---
+++ week-02/day-01/oldexercises/matchmaking/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> makingMatches(const std::vector<std::string>& girls, const std::vector<std::string>& boys)
+{
+    std::vector<std::string> result;
+    int size = girls.size() > boys.size() ? girls.size() : boys.size();
+    for (int i = 0; i < size; ++i) {
+        result.push_back(girls[i]);
+        result.push_back(boys[i]);
+    }
+}
+
+int main(int argc, char* args[])
+{
+    std::vector<std::string> girls = {"Eve","Ashley","Claire","Kat","Jane"};
+    std::vector<std::string> boys = {"Joe","Fred","Tom","Todd","Neef","Jeff"};
+
+    // Write a method that joins the two lists by matching one girl with one boy into a new list
+    // If someone has no pair, he/she should be the element of the list too
+    // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+    std::vector<std::string> matches =  makingMatches(girls, boys);
+
+    for(int i = 0; i < matches.size(); ++i) {
+        std::cout << matches[i] << " ";
+    }
+
+    return 0;
+}
