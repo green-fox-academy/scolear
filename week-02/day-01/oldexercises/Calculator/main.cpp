12f5e8fdcaa12b451903abe3690456f9e57bf17b
scolear <bobtera@gmail.com>
2020-01-30 22:37:03 +0100

    finish rickashley

---
+++ week-02/day-01/oldexercises/Calculator/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char* args[])
+{
+    // Create a simple calculator application which reads the parameters from the prompt
+    // and prints the result to the prompt.
+
+    // It should support the following operations: +, -, *, /, % and it should support two operands.
+    // The format of the expressions must be: {operation} {operand} {operand}.
+    // Examples:
+    // "+ 3 3" (3 + 3 -> the result will be 6)
+    // "* 4 4" (4 * 4 -> the result will be 16)
+    // "/ 20 8" (20 / 8 -> the result will be 2)
+    // "% 20 8" (20 % 8 -> the result will be 4)
+
+    // It should work like this:
+    // Start the program
+    // It prints: "Please type in the expression:"
+    // Waits for the user input
+    // Print the result to the prompt
+    // Exit
+
+
+    return 0;
+}
